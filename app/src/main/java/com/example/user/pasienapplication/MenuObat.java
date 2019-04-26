f001011e4f7110d5a89464ec7d124c5fe7122b8f
Kryuu <fernando.alan98@gmail.com>
2019-04-26 15:01:41 +0700

    Fitur Obat - Fernando

--- app/src/main/java/com/example/user/pasienapplication/MenuObat.java
+++ app/src/main/java/com/example/user/pasienapplication/MenuObat.java
@@ -1,20 +1,51 @@
 package com.example.user.pasienapplication;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MenuObat extends AppCompatActivity {
+
+    LinearLayout resep, scan, otc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_obat);
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setTitle("Buy Medicine");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Buy Medicine");
+
+//        resep = (LinearLayout) findViewById(R.id.resep);
+//        resep.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent =  new Intent(MenuObat.this, MenuResep.class);
+//                startActivity(intent);
+//            }
+//        });
+
+//        scan = (LinearLayout) findViewById(R.id.scan);
+//        scan.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent =  new Intent(MenuObat.this, MenuScan.class);
+//                startActivity(intent);
+//            }
+//        });
+
+        otc = (LinearLayout) findViewById(R.id.otc);
+        otc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =  new Intent(MenuObat.this, MenuOtc.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
