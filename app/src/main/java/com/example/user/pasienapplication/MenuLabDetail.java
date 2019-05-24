92d0fe4114059151c415878b62d37f3f6d50ed38
Kryuu <fernando.alan98@gmail.com>
2019-05-24 14:49:48 +0700

    Fitur Lab - Fernando

--- app/src/main/java/com/example/user/pasienapplication/MenuLabDetail.java
+++ app/src/main/java/com/example/user/pasienapplication/MenuLabDetail.java
@@ -1,13 +1,41 @@
 package com.example.user.pasienapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MenuLabDetail extends AppCompatActivity {
+
+    Button requestlab;
+    TextView detailprosedur;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_lab_detail);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Laboratorium");
+
+        requestlab = (Button) findViewById(R.id.requestlab);
+        requestlab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuLabDetail.this, PopOrder.class);
+                startActivity(intent);
+            }
+        });
+
+        detailprosedur = (TextView) findViewById(R.id.detailprosedur);
+        detailprosedur.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuLabDetail.this, PopDetailLab.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/user/pasienapplication/PopDetailLab.java
@@ -0,0 +1,14 @@
+package com.example.user.pasienapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PopDetailLab extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pop_detail_lab);
+
+    }
+}
