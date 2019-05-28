f324e56a27c18d35f81bced2e61a092963e3d07f
Kryuu <fernando.alan98@gmail.com>
2019-05-28 09:21:32 +0700

    Fitur Radiologi - Fernando

--- app/src/main/java/com/example/user/pasienapplication/MenuRadiologi.java
+++ app/src/main/java/com/example/user/pasienapplication/MenuRadiologi.java
@@ -5,8 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 
 import java.util.ArrayList;
 
@@ -14,6 +16,7 @@ public class MenuRadiologi extends AppCompatActivity {
 
     AutoCompleteTextView search_item;
     ArrayList<String> street = new ArrayList<>();
+    Button requestradio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +38,15 @@ public class MenuRadiologi extends AppCompatActivity {
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(MenuRadiologi.this,android.R.layout.simple_spinner_dropdown_item,street);
         search_item.setAdapter(adapter);
+
+        requestradio = (Button) findViewById(R.id.requestradio);
+        requestradio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuRadiologi.this, PopOrder.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
