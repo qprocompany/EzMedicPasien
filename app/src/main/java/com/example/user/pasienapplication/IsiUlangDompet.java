b30148c815c4a4d67976b6a9c611e33e81d01600
bagasdp <bagasdwip@ymail.com>
2019-05-29 16:18:46 +0700

    update : isi dompet

--- app/src/main/java/com/example/user/pasienapplication/IsiUlangDompet.java
+++ app/src/main/java/com/example/user/pasienapplication/IsiUlangDompet.java
@@ -6,12 +6,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class IsiUlangDompet extends AppCompatActivity {
 
     RadioGroup radioGroup;
     Button button;
+    RadioButton radioButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,14 +24,31 @@ public class IsiUlangDompet extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Isi Ulang");
 
-        radioGroup = (RadioGroup) findViewById(R.id.RadioNominal);
 
+
+        radioGroup = (RadioGroup) findViewById(R.id.RadioNominal);
         button = (Button) findViewById(R.id.btnTambahNominal);
+
+
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(IsiUlangDompet.this, MenuMore.class);
                 startActivity(intent);
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int selectedId = radioGroup.getCheckedRadioButtonId();
+
+                // find the radiobutton by returned id
+                radioButton = (RadioButton) findViewById(selectedId);
+
+                Toast.makeText(IsiUlangDompet.this,
+                        radioButton.getText(), Toast.LENGTH_SHORT).show();
             }
         });
     }
